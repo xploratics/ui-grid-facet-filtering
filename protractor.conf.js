db6b4e15127531ff0c87655d9993a85b876fd89b
Dany Laporte <dany_laporte@hotmail.com>
2016-09-18 17:29:37 -0400

    add express to serve page for testing

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,4 +1,40 @@
 exports.config = {
-  seleniumAddress: 'ondemand.saucelabs.com:80',
-  specs: ['test/e2e/*.js']
-};
+  sauceUser: process.env.SAUCE_USERNAME,
+  sauceKey: process.env.SAUCE_ACCESS_KEY,
+
+  //seleniumAddress: 'http://ondemand.saucelabs.com:80/wd/hub',
+  specs: ['test/e2e/*.js'],
+
+  // restartBrowserBetweenTests: true,
+
+  beforeLaunch: function () {
+      var express = require('express');
+      var app = express();
+      
+      app.use(express.static('.'));
+      app.listen(8888);
+  },
+
+  onPrepare: function() {
+      var caps = browser.getCapabilities()
+  },
+
+  multiCapabilities: [{
+    browserName: 'firefox',
+    version: 'dev',
+    platform: 'Windows 10',
+    name: "firefox-tests",
+    shardTestFiles: true,
+    maxInstances: 25
+  }, {
+    browserName: 'chrome',
+    version: '53',
+    platform: 'Windows 10',
+    name: "chrome-tests",
+    shardTestFiles: true,
+    maxInstances: 25
+  }],
+
+  onComplete: function() {
+  }
+}
--- test/e2e/facet.js
+++ test/e2e/facet.js
@@ -1,5 +1,5 @@
 describe('facet-filtering', function () {
     it('it should be empty when nothing selected', function () {
-        browser.get('http://localhost:8888');
+        browser.get('http://localhost:8888/app/index.html');
     });
 });
